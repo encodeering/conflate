db3bbb9a44003dd7d278ce353a994a71c395014e
Michael Clausen <encodeering@gmail.com>
2017-02-08 20:09:36 +0100

    defines a few mockito helpers
    * when is a keyword, uses whenever here
    
    NOTE
    
    * https://medium.com/@juanchosaravia/keddit-part-9-unit-test-with-kotlin-mockito-spek-76709812e3b6#.5p1rbgaef

---
+++ src/test/kotlin/com/encodeering/conflate/fixture/Spy.kt
@@ -0,0 +1,14 @@
+package com.encodeering.conflate.fixture
+
+import org.mockito.Mockito
+
+/**
+ * @author Michael Clausen - encodeering@gmail.com
+ */
+inline fun         <T> eq (value : T) : T = Mockito.eq (value)
+
+inline fun <reified T> any () : T = Mockito.any (T::class.java)
+
+inline fun <reified T> mock () : T = Mockito.mock (T::class.java)
+
+       fun <T> whenever (element : T) = Mockito.`when` (element)
