efc35c4392076b212cca569b0243ebcf8e7b127f
Michael Clausen <encodeering@gmail.com>
2017-02-10 18:06:25 +0100

    defines a throws assertion
    * following a suggestion from kotlin test with a reified syntax
    
    NOTE
    
    * https://github.com/JetBrains/spek/issues/89

---
+++ src/test/kotlin/com/encodeering/conflate/fixture/Assertion.kt
@@ -0,0 +1,22 @@
+package com.encodeering.conflate.fixture
+
+/**
+ * @author Michael Clausen - encodeering@gmail.com
+ */
+inline fun <reified T : Throwable> throws (block : () -> Unit) {
+    var ex : Throwable? = null
+    var thrown  = false
+    var matches = false
+
+    try {
+        block ()
+    } catch                           (e : Throwable) {
+        ex =                           e
+        matches = T::class.isInstance (e)
+        thrown  = true
+
+    } finally {
+        if (! matches && ex != null) throw AssertionError ("block should have thrown a ${T::class.simpleName}, but threw a ${ex.javaClass.simpleName}")
+        if (! thrown) throw AssertionError ("block should have thrown a ${T::class.simpleName}")
+    }
+}
