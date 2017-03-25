e9fed56d90aba9b2a9d37139e7f037bb38813c99
Michael Clausen <encodeering@gmail.com>
2017-03-25 12:49:23 +0100

    adds a test utility to create simple spy callbacks
    * useful for simple has-been-called semantic

--- modules/conflate-test/src/main/kotlin/com/encodeering/conflate/test/Spy.kt
+++ modules/conflate-test/src/main/kotlin/com/encodeering/conflate/test/Spy.kt
@@ -12,3 +12,7 @@ inline fun <reified T> any () : T = Mockito.any (T::class.java)
 inline fun <reified T> mock () : T = Mockito.mock (T::class.java)
 
        fun <T> whenever (element : T) = Mockito.`when` (element)
+
+       fun spy () = mock<() -> Unit> ()
+
+inline fun <reified T> spy () = mock<(T) -> Unit> ()
